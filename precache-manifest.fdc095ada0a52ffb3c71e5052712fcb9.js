dfd57489803d43cdc51a49b3a72741385d2ee436
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-05-14 09:17:02 -0600

    Updates

---
+++ precache-manifest.fdc095ada0a52ffb3c71e5052712fcb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e70da0ed1acb01f057e",
+    "url": "/SGPortfolio/static/css/main.e2795736.chunk.css"
+  },
+  {
+    "revision": "2e70da0ed1acb01f057e",
+    "url": "/SGPortfolio/static/js/main.2e70da0e.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/SGPortfolio/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "c370bbf5c1f66d792a58",
+    "url": "/SGPortfolio/static/js/runtime~main.c370bbf5.js"
+  },
+  {
+    "revision": "4e766ba966a129b2d99ce582533bb1cf",
+    "url": "/SGPortfolio/static/media/GS.4e766ba9.png"
+  },
+  {
+    "revision": "308b55c6aa94d76125ac6c808895cfa4",
+    "url": "/SGPortfolio/static/media/HPE.308b55c6.png"
+  },
+  {
+    "revision": "29eb36016185477533c25b7f338fdb7c",
+    "url": "/SGPortfolio/static/media/DU.29eb3601.jpg"
+  },
+  {
+    "revision": "a40716601f255de5fdd4e7432bb9e7d5",
+    "url": "/SGPortfolio/static/media/CSU.a4071660.jpg"
+  },
+  {
+    "revision": "a81df4dd6102a2ed6b42f3ab28d1e4c7",
+    "url": "/SGPortfolio/index.html"
+  }
+];
